c3de15190d7dd6a1f9bdb49c41ef75c36aeac1ae
Érica <ericaguimaraes@engineer.com>
2022-08-24 19:38:32 -0300

    1 programação teste

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
 let nome = "erica" 
+console.log nome
 let email = "ericaguimaraes@engineer.com"
+console.log email
 let profissao = "engenharia"
+console.log profissao
 let idade = 31
+console.log idade
 let prox destino = "cape town"
+console.log prox destino
